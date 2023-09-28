769cbe8bdc3bc810a47f7f8d1f3f0e9e16cb7c87
MuhammadAmmar7704 <115612217+MuhammadAmmar7704@users.noreply.github.com>
2023-09-28 23:01:57 +0500

    Add files via upload

---
+++ x_y_both_working.py
@@ -0,0 +1,27 @@
+from PIL import Image, ImageDraw, ImageFont
+import openpyxl
+import pandas
+from openpyxl import Workbook, load_workbook
+# book = load_workbook('menu.xlsx')
+df = pandas.read_excel('menu.xlsx')
+# dl = df.parse("Sheet1")
+
+# sheet = book.active
+# data = pandas.DataFrame(df, columns = ['ABCD'])
+# print(img.size)
+data = df['ABCD'][0]
+# print(df.width)
+fnt = ImageFont.truetype("consola.ttf", 40)
+# fnt = ImageFont.truetype("corbeli.ttf", 19)
+# names = sheet['A']
+i = int(1)
+# print(data[0].value)
+for cell in df['ABCD']:
+    img = Image.open('b.png')
+    d = ImageDraw.Draw(img)
+    x = (img.width - (d.textlength(cell, font=fnt))) // 2
+    y = (img.height - (d.textlength(cell, font=fnt))) // 2
+    # d.text((160, 183), sheet['A'+str(i+1)].value, font = fnt, fill = (0, 0, 0))
+    d.text((x, 380), cell, font = fnt, fill = (0, 0, 0))
+    img.save('bb'+str(i+1)+'.png')
+    i = i + 1
